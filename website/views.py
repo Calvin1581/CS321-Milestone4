9f4a99d3ffb2570af7ba10ca5bd84bdd7d5f4762
Milo Lani-Caputo <mlanicaputo@gmail.com>
2022-10-24 16:47:03 -0400

    Add page methods to views

--- website/views.py
+++ website/views.py
@@ -4,8 +4,62 @@ from flask_login import login_required, current_user
 from . import db
 from .models import Note
 
+
 views = Blueprint('views', __name__)
+
 
 @views.route("/")
 def home():
-    return "<h1>Howdy milestone 4ers</h1>"
+    return render_template("index.html")
+
+
+@views.route("/coach-dashboard")
+def coach_dashboard():
+    return render_template("coachDashboard.html")
+
+
+@views.route("/coach-athlete-view")
+def coach_athlete_view():
+    return render_template("coachAthleteView.html")
+
+
+@views.route("/athlete-nutrition")
+def athlete_nutrition_data():
+    return render_template("athleteNutritionData.html")
+
+
+@views.route("/athlete-readiness")
+def athlete_readiness_data():
+    return render_template("athleteReadinessData.html")
+
+
+@views.route("/athlete-sleep")
+def athlete_sleep_data():
+    return render_template("athleteSleepData.html")
+
+
+@views.route("/athlete-dashboard")
+def athlete_dashboard():
+    return render_template("athleteDashboard.html")
+
+
+@views.route("/admin-dashboard")
+def admin_dashboard():
+    return render_template("adminPeakDashboard.html")
+
+
+@views.route("/admin-team-view")
+def admin_team_view():
+    return render_template("adminPeakTeamView.html")
+
+
+@views.route("/admin-athlete-view")
+def admin_athlete_view():
+    return render_template("adminPeakAthleteView.html")
+
+
+@views.route("/admin-permissions")
+def admin_permissions():
+    return render_template("permissions.html")
+
+
