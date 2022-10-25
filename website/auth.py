028ce0855630d074d4528a7eb8142274eec1d1e0
benraivel <47224831+benraivel@users.noreply.github.com>
2022-10-25 13:41:55 -0400

    added unified coach/athlete dashboard

--- website/auth.py
+++ website/auth.py
@@ -20,7 +20,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.coachDashboard'))
+                return redirect(url_for('views.dashboard'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -41,6 +41,8 @@ def sign_up():
 
     if request.method == 'POST':
         email = request.form.get('email')
+        first_name = request.form.get('first-name')
+        last_name = request.form.get('last-name')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm-password')
         role = request.form.get('role')
@@ -57,11 +59,14 @@ def sign_up():
             flash('Passwords don\'t match.', category='error')
         else:
             # add user to database
-            new_user = User(email=email, password=generate_password_hash(password, method='sha256'), role=role)
+            new_user = User(email=email,
+                            password=generate_password_hash(password, method='sha256'),
+                            role=role, first_name=first_name, last_name=last_name)
+
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.coachDashboard'))
+            return redirect(url_for('views.dashboard'))
 
     return render_template("signup.html", user=current_user)
