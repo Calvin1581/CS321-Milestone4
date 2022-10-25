94e66b4b9c515e09732d824cd76a0b0a4ad3586d
Nicole Matamoros <113453620+nimata23@users.noreply.github.com>
2022-10-25 16:31:36 -0400

    added view for upload files page, started adding code for when someone tries to upload a csv

--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from urllib import request
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from os.path import join, dirname, realpath
 from . import db
 from .models import Note
+from .csvParse import parseCSV
+
 
 views = Blueprint('views', __name__)
 
@@ -29,3 +32,16 @@ def permissions():
 @views.route("/signup")
 def signUp():
     return render_template("signup.html")
+
+@views.route("/upload", methods = ['GET','POST'])
+def uploadFiles():
+    #get uploaded file
+    if request.method == 'POST':
+
+        uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'],uploaded_file.filename)
+        #set the file path
+        uploaded_file.save(file_path)
+        parseCSV(file_path)
+    return render_template('upload.html')
