36d742ac03dfa2394fcbb362323fb41ce470c251
tamsinrogers <tamsinrogers@icloud.com>
2022-10-25 21:35:34 -0400

    import plotly

--- website/views.py
+++ website/views.py
@@ -4,6 +4,10 @@ from flask_login import login_required, current_user
 from . import db
 from .models import Note
 
+import json
+import plotly
+import plotly.express as px
+
 views = Blueprint('views', __name__)
 
 
@@ -25,3 +29,13 @@ def coachDashboard():
 @views.route("/permissions")
 def permissions():
     return render_template("permissions.html")
+
+@views.route('/figures', methods=['GET', 'POST'])
+@login_required
+def figures():
+
+    df = px.data.medals_wide()
+    fig = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="Medals", barmode="group")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("figures.html", user=current_user, graphJSON=graphJSON)
