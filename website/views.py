ad07e16139bddc52b8e44ef67eff8fad488d87ff
tamsinrogers <tamsinrogers@icloud.com>
2022-10-25 23:55:12 -0400

    first chart done

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from urllib import request
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from . import db
-from .models import Note
+#from . import db
+#from .models import Note
+import pandas as pd
 
 import json
 import plotly
@@ -23,19 +24,23 @@ def athleteDashboard():
 
 @views.route("/coachDashboard")
 def coachDashboard():
-    return render_template("coachDashboard.html", user=current_user)
+    
+    df = pd.read_csv('website/data/sleep.csv')
+    hoursAvg = df["Hours"].mean().astype(int)
 
+    labels = ["Hours"]
+    values = [hoursAvg]
+    
+    fig = px.pie(labels, values = values, hole = 0.4, width=280, height=280, color = labels, color_discrete_map = {'Hours':'blue'})
+
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0,0,0,0)', 
+        'paper_bgcolor': 'rgba(0,0,0,0)',
+        })
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("coachDashboard.html", user=current_user, graphJSON=graphJSON)
 
 @views.route("/permissions")
 def permissions():
     return render_template("permissions.html")
-
-@views.route('/figures', methods=['GET', 'POST'])
-@login_required
-def figures():
-
-    df = px.data.medals_wide()
-    fig = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="Medals", barmode="group")
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return render_template("figures.html", user=current_user, graphJSON=graphJSON)
