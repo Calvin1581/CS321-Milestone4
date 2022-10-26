5372776518a675a459f13e6011f844bcc9f20cf5
benraivel <47224831+benraivel@users.noreply.github.com>
2022-10-26 18:18:16 -0400

    removed unused import x2

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,3 @@
-from tarfile import LENGTH_LINK
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User, Note
 from werkzeug.security import generate_password_hash, check_password_hash
