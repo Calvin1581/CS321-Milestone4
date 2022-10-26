7b012adec75b83397970fc8f6a0ca040b694744e
Milo Lani-Caputo <mlanicaputo@gmail.com>
2022-10-26 16:28:35 -0400

    Add uploading support for csvs

--- website/__init__.py
+++ website/__init__.py
@@ -2,16 +2,21 @@ from venv import create
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from pathlib import Path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+# the location of the csv upload folder
+UPLOAD_FOLDER = "./csvs"
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # configure an upload folder for csv uploads
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     db.init_app(app)
 
     from .views import views
--- website/views.py
+++ website/views.py
@@ -4,6 +4,11 @@ from flask_login import login_required, current_user
 from . import db
 from .models import Note
 
+import os
+import sys
+from flask import Flask, flash, request, redirect, url_for, current_app
+from werkzeug.utils import secure_filename
+
 views = Blueprint('views', __name__)
 
 
@@ -25,3 +30,43 @@ def coachDashboard():
 @views.route("/permissions")
 def permissions():
     return render_template("permissions.html")
+
+
+# helper function for upload()
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in {"csv"}
+
+@views.route("/upload", methods=["GET", "POST"])
+def upload():
+    sys.stdout.flush()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        sys.stdout.flush()
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for("views.upload"))
+
+    # basic, flask-provided html for uploading files
+    # TODO make an actual webpage that imlements this
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
